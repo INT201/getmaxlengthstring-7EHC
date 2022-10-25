a1498704adbff0380420641d355c71d6a65d54f2
Purin <purin.peera@mail.kmutt.ac.th>
2022-10-25 22:47:49 +0700

    fins

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,5 +2,20 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  let maxlength = 0
+  let maxarr = []
+  if(arrayOfString === null || arrayOfString === undefined) {
+      return undefined
+  } else {
+  for (let string of arrayOfString) {
+    if (string.length > maxlength) {
+      maxlength = string.length;
+      maxarr = [string];
+    } else if (string.length === maxlength) {
+      maxarr.push(string);
+    }
+  }
+  return maxarr;
+}
 }
 module.exports = getMaxLengthString
